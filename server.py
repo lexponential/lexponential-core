66e3cf49e5d5efc0c314da653bd098b2591af45b
lucaswadedavis <lucaswadedavis@gmail.com>
2016-04-17 12:20:11 -0700

    modify boilerplate Flask project for use with Lexponential

--- server.py
+++ server.py
@@ -9,68 +9,42 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 db = SQLAlchemy(app)
 
-class Node(db.Model):
+
+class Lexeme(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    node_type = db.Column(db.String(80))
-    value = db.Column(db.Text)
+    lexeme = db.Column(db.Text)
+    from_language = db.Column(db.String(80))
+    to_language = db.Column(db.String(80))
 
-    def __init__(self, node_type, value):
-        self.node_type = node_type
-        self.value = value
+    def __init__(self, lexeme, from_language, to_language):
+        self.lexeme = lexeme
+        self.from_language = from_language
+        self.to_language = to_language
 
-class Edge(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    edge_type = db.Column(db.String(80))
-    from_node = db.Column(db.Text)
-    to_node = db.Column(db.Text)
-
-    def __init__(self, edge_type, from_node, to_node):
-        self.edge_type = edge_type
-        self.from_node = from_node
-        self.to_node = to_node
 
 # the base route which renders a template
 @app.route('/')
 def index ():
     return render_template('index.html')
 
-@app.route('/nodes/<node_type>')
-def get_nodes (node_type):
-    # will return a list of all nodes of a given node_type
-    nodes = Node.query.all()
-    return jsonify({'nodes': []})
 
-# create a node
-@app.route('/nodes/create', methods=['POST'])
-def create_node ():
+@app.route('/lexemes/<lexeme>')
+def get_lexemes (lexeme):
+    # will return a list of all lexemes of a given lexeme
+    lexemes = Lexeme.query.all()
+    return jsonify({'lexemes': []})
+
+
+# create a lexeme
+@app.route('/lexemes/create', methods=['POST'])
+def create_lexeme ():
     payload = request.get_json()
+    lexemes = payload['lexemes']
     # guard clause to  ensure value is valid
-    node = Node('knowleton', payload['value'])
-    db.session.add(node)
+    lexeme = Lexeme('dog', 'english', 'spanish')
+    db.session.add(lexeme)
     db.session.commit()
     return jsonify(payload)
-
-@app.route('/edges/<edge_type>')
-def get_edges (edge_type):
-    # will return a list of all edges of a given edge_type
-    edges = Edge.query.all()
-    return jsonify({'edges': []})
-
-# create a node
-@app.route('/edges/create', methods=['POST'])
-def create_edge ():
-    payload = request.get_json()
-    # guard clause to  ensure values are valid
-    from_node = payload['from_node']
-    to_node = payload['to_node']
-    edge = Edge('depends_on', from_node, to_node)
-    db.session.add(edge)
-    db.session.commit()
-    return jsonify(payload)
-
-
-# create an edge
-
 
 
 if __name__ == '__main__':
